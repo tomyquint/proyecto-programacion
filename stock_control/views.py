25df4248aeb921e3f23f26227fe9750fdce5f0c8
Tomás Quinteros <quinterost03@gmail.com>
2020-10-05 15:57:53 -0300

    HTML modificado.

--- stock_control/views.py
+++ stock_control/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Producto
 
 # Create your views here.
 def lista_productos(request):
-    return render(request, 'stock_control/lista_productos.html', {})
+    productos = Producto.objects.all()
+    return render(request, 'stock_control/lista_productos.html', {'productos': productos})
